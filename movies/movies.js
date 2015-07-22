ef6c9a800a0dbe31921f7598a6c3dd5fb4b88c7a
Saira J. Barlas <c12809080@mydit.ie>
2015-07-22 11:23:01 +0100

    Create movies.js

---
+++ movies/movies.js
@@ -0,0 +1,9 @@
+// Movies module
+// Object factory - an object which is responsible for creating other objects
+module.exports = function() {
+	return {
+		favMovies: "";
+	}
+}
+
+// Each module can have their own copy of anything we want
