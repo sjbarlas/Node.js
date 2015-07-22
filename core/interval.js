2bbd19a530cdc59464eebd15a6ce4d02f3749f78
Saira J. Barlas <c12809080@mydit.ie>
2015-07-22 11:50:19 +0100

    Create interval.js

---
+++ core/interval.js
@@ -0,0 +1,8 @@
+setInterval(function() {
+
+	console.log('beef');
+
+}, 2000); // every 2 seconds, any code that is in the call back will be called
+
+// Timeout runs once after a period of interval
+// Intervals runs repeatedly
